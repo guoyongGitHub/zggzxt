60f7c3dc058265c7941e7c49fcb623ee93fbc429
guoyongGitHub <40432498+guoyongGitHub@users.noreply.github.com>
2018-06-29 11:52:18 +0800

    Update 职工管理系统.cpp

--- 职工管理系统.cpp
+++ 职工管理系统.cpp
@@ -1,10 +1,10 @@
+#include<stdio.h>
 #include<iostream>
 #include<fstream>
 #include<string>
 #include<iomanip>
 using namespace std;
 void read(); //读取
-void write();//保存
 void find();//查询
 void list();//浏览
 void modify();//修改
@@ -13,7 +13,6 @@ void add();//添加
 int grsds(float a);//计算个人所得税
 void menu();//主界面
 int number,oup;//职工总数
-	
 struct zggz
 {  
 		int gongh;
@@ -22,12 +21,13 @@ struct zggz
 		float wage2;        //薪级工资
 		float wage3;		//职务津贴
 		float wage4;        //绩效工资
-                float wage5;        //应发工资
+        float wage5;        //应发工资
 		float wage6;        //个人所得税
 		float wage7;        //实发工资
 };
 zggz zggz0[50];
-
+zggz *p;
+void write(zggz *p,int j);//保存
 
 void main()
 	{
@@ -39,26 +39,30 @@ void menu()
 		int n,w1;
 		do
 		{
-			
-		
-			cout<<"<------------职工工资管理系统------------>"<<endl;
-			cout<<"|                 1.读取                 |"<<endl;
-			cout<<"|                 2.保存                 |"<<endl;
-			cout<<"|                 3.查询                 |"<<endl;
-			cout<<"|                 4.浏览                 |"<<endl;
-			cout<<"|                 5.修改                 |"<<endl;
-			cout<<"|                 6.删除                 |"<<endl;
-			cout<<"|                 7.添加                 |"<<endl;
-			cout<<"|                 8.退出                 |"<<endl;
-			cout<<"<---------------------------------------->"<<endl;
-			cout<<"              请输入你的选择：            "<<endl;
+			printf("\n");
+			printf("\n");
+			printf("###\t欢迎使用广西民族大学软件与信息安全学院职工工资管理系统\t###\n");
+			printf("\n");
+			printf("\n");
+		    printf("请选择（1-7）：\n");
+			printf("================================================================\n");
+			printf("|           1.查询职工工资记录                                 |\n");
+			printf("|           2.修改职工工资记录                                 |\n");
+			printf("|           3.添加职工工资记录                                 |\n");
+			printf("|           4.删除职工工资记录                                 |\n");
+			printf("|           5.保存数据到文件                                   |\n");
+			printf("|           6.浏览职工记录                                     |\n");
+			printf("|           7.退出系统                                         |\n");	
+			printf("================================================================\n");
+			printf("\n");
+			printf("你的选择是:\n");
  
 
-		cin>>n;
-		if(n<1||n>9)
+		scanf("%d",&n);
+		if(n<1||n>7)
 		{
 			w1=1;
-            cout<<"你的选择不再范围内，请重新输入"<<endl;
+            printf("你的选择不再范围内，请重新输入\n");
 			getchar();
 		}
 		else w1=0;
@@ -66,15 +70,20 @@ void menu()
 		while(w1==1);
 switch(n)
 	{
-		case 1:read(); break;
-	
-    	case 2:find(); break;
-		case 3:list(); break;
-		case 4:modify(); break;
-		//case 5:del(); break;
-		case 6:add(); break;
-    	case 7:cout<<"谢谢，欢迎下次启动"<<endl;
-		default:exit(0);       }//退出系统
+		case 1:find();break;
+    	case 2:modify(); break;
+		case 3:add(); break;
+		case 4:del(); break;
+		case 5:write(::p,::oup); break;
+		case 6:list(); break;
+    	case 7:printf("谢谢，欢迎下次启动\n");
+		default:exit(0);      
+			
+}//退出系统
+        
+
+read();
+		
 
 }
 
@@ -84,7 +93,7 @@ void read()
 	ifstream in("gz.dat",ios::binary);
 if(!in)
 {
-cout<<"Cannot open output file.\n";
+printf("Cannot open output file.\n");
 	abort();  //退出程序
 }
 in.read((char *)&::zggz0,sizeof(::zggz0));
@@ -96,17 +105,17 @@ for(int n=0;n<50;n++)
 	else
 		break;
 	}
+menu();
 	
 }
-	/*	cout<<zggz0[i].gongh<<"  "<<zggz0[i].name<<"  "<<zggz0[i].wage1<<"  "<<zggz0[i].wage2<<"  "
-			<<zggz0[i].wage3<<"  "<<zggz0[i].wage4<<"  "<<zggz0[i].wage5<<"  "<<zggz0[i].wage6<<"  "<<zggz0[i].wage7<<endl;*/
+	
 
 void write(zggz *p,int j)
 {
 	ofstream out("gz.dat",ios::binary);
 	if(!out)
 	{
-		cout<<"Cannot open out put file"<<endl;
+		printf("Cannot open out put file");
 		abort();
 	}
     for(int i=0;i<j+1;i++)
@@ -115,40 +124,66 @@ void write(zggz *p,int j)
 		p++;
     }
 	out.close();
+	printf("正在保存数据\n");
+	printf("为你返回主菜单\n");
+	read();
+	
 }
-
 
 
 
 
 void find()
 {
-  read();
-	 
-	      int j;
-		  cout<<"请输入你要查询的工号："<<endl;
-		  cin>>j;
-		  if(1<=j&&j<=::number)
-		  { cout<<"工号  姓名  岗位工资  薪级工资  职务津贴  效绩工资  应发工资  实发工资"<<endl;
-		   	cout<<::zggz0[j-1].gongh<<" "<<::zggz0[j-1].name<<" "<<::zggz0[j-1].wage1<<" "<<::zggz0[j-1].wage2
-			<<" "<<::zggz0[j-1].wage3<<" "<<::zggz0[j-1].wage4<<" "<<::zggz0[j-1].wage5<<" "<<zggz0[j-1].wage6<<endl;  
+  
+	      int k,i=0,w;
+          loop:printf("请输入你要查询的工号");//cout<<"请输入你要查询的工号："<<endl;
+		  scanf("%d",&k);
+		  for(int j=0;j<::number;j++)
+		  {    
+			  
+				 if (k==::zggz0[j].gongh)
+				 {
+		   
+			printf("工号  姓名  岗位工资  薪级工资  职务津贴  效绩工资  应发工资  实发工资\n");
+		   	printf("%d     %s      %g      %g      %g      %g      %g      %g\n",::zggz0[j].gongh,::zggz0[j].name,
+				::zggz0[j].wage1,::zggz0[j].wage2,::zggz0[j].wage3,::zggz0[j].wage4,::zggz0[j].wage5,zggz0[j].wage7);  
+		    i=k;
+				 printf("重新查询请按1，任意键返回菜单\n");
+				 scanf("%d",&w);
+			     if(w==1)
+				   goto loop;
+			     else 
+				   menu();
+				 }
 		  }
-	   else
-		   cout<<"没有这一项数据"<<endl;
+		  if(i!=k)
+		  {
+		   printf("没有这个职员的信息，重新查询请按1，任意键返回菜单\n");
+		   scanf("%d",&w);
+			   if(w==1)
+				   goto loop;
+			   else 
+				   menu();
+		  }
 	   
 
+          
 
 }
 
 void list()
 {
      //read();
-     cout<<"工号  姓名  岗位工资  薪级工资  职务津贴  效绩工资  应发工资  实发工资"<<endl;
-	 cout<<::number<<endl;
+     printf("现存有 %d",::number);
+	 printf("条员工信息\n");
+     printf("\n");
+     printf("工号  姓名  岗位工资  薪级工资  职务津贴  效绩工资  应发工资  实发工资\n");
+	
 	for(int i=0;i<::number;i++)
 	{ 
-		cout<<::zggz0[i].gongh<<" "<<::zggz0[i].name<<" "<<::zggz0[i].wage1<<" "<<::zggz0[i].wage2<<" "
-			<<::zggz0[i].wage3<<" "<<::zggz0[i].wage4<<" "<<::zggz0[i].wage5<<" "<<zggz0[i].wage6<<endl;	
+	printf("%d     %s      %g      %g      %g      %g      %g      %g\n",::zggz0[i].gongh,::zggz0[i].name,::zggz0[i].wage1,::zggz0[i].wage2
+			,::zggz0[i].wage3,::zggz0[i].wage4,::zggz0[i].wage5,zggz0[i].wage7);  
 	} 
 menu();
 
@@ -161,59 +196,105 @@ void modify()
 
           int k,i=0,w,a1,a2,a3,a4;
 		  zggz *q;
-          cout<<"请输入你要修改的工号："<<endl;
-		  cin>>k;
+          loop:printf("请输入你要修改的工号：\n");
+		  scanf("%d",&k);
 		  for(int j=0;j<::number;j++)
 		  {    
 			  
 		 if (k==::zggz0[j].gongh)
 		 {
-		 cout<<"职员信息如下："<<endl;
-		 cout<<"工号"<<"\t"<<"姓名"<<"\t"<<"岗位工资"<<"\t"<<"薪级工资"<<"\t"<<"职务工资"<<"\t"<<"效绩工资"<<"\t"<<"应发工资"<<"\t"<<"实发工资"<<endl;
-		 cout<<::zggz0[j].gongh<<"\t"<<::zggz0[j].name<<"\t"<<::zggz0[j].wage1<<
-			 "\t\t"<<::zggz0[j].wage2<<"\t\t"<<::zggz0[j].wage3<<"\t\t"<<::zggz0[j].wage4<<"\t\t"<<::zggz0[j].wage5<<"\t\t"<<zggz0[j].wage6<<endl;
-	     cout<<"修改后的岗位工资："<<"  "; 
-         cin>>a1;
-		 cout<<"修改后的薪级工资："<<"  ";
-		 cin>>a2;
-		 cout<<"修改后的职务津贴："<<"  ";
-		 cin>>a3;
-		 cout<<"修改后的效绩工资："<<"  ";
-		 cin>>a4;
+		 printf("职员信息如下：\n");
+		 printf("工号    姓名     岗位工资      薪级工资     职务工资     效绩工资     应发工资     实发工资\n");
+
+		 printf("%d     %s      %g      %g      %g      %g      %g      %g\n",::zggz0[j].gongh,::zggz0[j].name,::zggz0[j].wage1,::zggz0[j].wage2
+			,::zggz0[j].wage3,::zggz0[j].wage4,::zggz0[j].wage5,zggz0[j].wage7,"\n");  
+	     printf("修改后的岗位工资：\t\n"); 
+         scanf("%d",&a1);
+		 printf("修改后的薪级工资：\t\n");
+		 scanf("%d",&a2);
+		 printf("修改后的职务津贴：\t\n");
+		 scanf("%d",&a3);
+		 printf("修改后的效绩工资：\t\n");
+		 scanf("%d",&a4);
+		  i=j;
 		 ::zggz0[j].wage1=a1;
-		 ::zggz0[j].wage2=a1;
-		 ::zggz0[j].wage3=a1;
-		 ::zggz0[j].wage4=a1;
-		 }    
-		 }
-		  q=::zggz0;
-		 cout<<"修改成功，保存（1），菜单（任意键）"<<endl;
-		 cin>>w;
+		 ::zggz0[j].wage2=a2;
+		 ::zggz0[j].wage3=a3;
+		 ::zggz0[j].wage4=a4;
+		 ::zggz0[j].wage5=a1+a2+a3+a4;
+		 ::zggz0[j].wage6=grsds(::zggz0[j].wage5);
+		 ::zggz0[j].wage7=::zggz0[j].wage5-::zggz0[j].wage6;
+		 q=::zggz0;
+		 printf("修改成功，保存请按1，任意键返回菜单\n");
+		 scanf("%d",&w);
 		 if(w==1)
 			 write(q,::oup);
 		 else
-		 menu();
+			 menu();
+		 }
+		  }
+			   if(i!=j)
+		  {
+		   printf("没有这个职员的信息，重新查询（1），菜单（任意键）\n");
+		   scanf("%d",&w);
+			   if(w==1)
+				   goto loop;
+			   else 
+				   menu();
+		  }
+			 
 }
+
+
+void del()
+{
+	::p=::zggz0;
+	 int gonghao;
+	 cout<<"请输入要删除的员工工号："<<endl;
+	 cin>>gonghao;
+	 for(int i=0;i<::number;i++)
+	 {
+		 if(gonghao==::zggz0[i].gongh)
+		 {
+			 if(i!=::number-1)
+			 {
+				 for(;i<::number;i++)
+				 {
+					 ::zggz0[i]=::zggz0[i+1];
+				 }
+				 ::number=::number-1;
+			 }
+			 else
+			 {
+				 ::number=::number-1;
+			 }
+		 }
+		 }
+	 write(::p,::number);
+}//删除*/
 
 
 void add()
 {
 	int i;
-	zggz zggz1[50],*p;
-	p=zggz1;
-	//read();
+	zggz zggz1[50];
+	::p=zggz1;
 	for(int j=0;j<::number;j++)
 	{
-	 zggz1[j]=zggz0[j];
-	
+	 zggz1[j]=zggz0[j];	
 	}
 	for(i=::number;i<50;i++)
 	{
 	int gongh1;
     char name1[20];
     float wage01,wage02,wage03,wage04;
-	cout<<"请按顺序输入工号  姓名  岗位工资  薪级工资  职务津贴  效绩工资"<<endl;
-    cin>>gongh1>>name1>>wage01>>wage02>>wage03>>wage04;
+	printf("请按顺序输入工号  姓名  岗位工资  薪级工资  职务津贴  效绩工资\n");
+    scanf("%d",&gongh1);
+	scanf("%s",name1);
+	scanf("%f",&wage01);
+	scanf("%f",&wage02);
+	scanf("%f",&wage03);
+	scanf("%f",&wage04);
 	zggz1[i].gongh=gongh1;
 	strcpy(zggz1[i].name,name1);
 	zggz1[i].wage1=wage01;
@@ -223,58 +304,45 @@ void add()
 	zggz1[i].wage5=wage01+wage02+wage03+wage04;
 	zggz1[i].wage6=grsds(zggz1[i].wage5);
 	zggz1[i].wage7=zggz1[i].wage5-zggz1[i].wage6;
-    cout<<"是否继续添加？(0(否)/1(是))"<<endl;
+	::oup=i;
+    printf("是否继续添加？(0(否)/1(是))\n");
 	int k;
-	cin>>k;
+	scanf("%d",&k);
 	if(k==0)
 	{
 		break;
-	}
-
-	}
-	write(p,i);
-     menu();
-
-}
-void grsds(int i)//求职工应发工资，个人所得税，实发工资
-{
-		zggz0[i].wage5=zggz0[i].wage1+zggz0[i].wage2+zggz0[i].wage3+zggz0[i].wage4;//计算应得工资
-		if(zggz0[i].wage5>100000)//计算不同工资的对应个人所得税
-		{
-			zggz0[i].wage6=(zggz0[i].wage5-100000)*0.45+8000+7000+6000+5000+3000+450+150+25;
-		}
-		else if(zggz0[i].wage5>80000)
-		{
-			zggz0[i].wage6=(zggz0[i].wage5-80000)*0.4+7000+6000+5000+3000+450+150+25;
-		}
-		else if(zggz0[i].wage5>60000)
-		{
-			zggz0[i].wage6=(zggz0[i].wage5-60000)*0.35+6000+5000+3000+450+150+25;
-		}
-		else if(zggz0[i].wage5>40000)
-		{
-			zggz0[i].wage6=(zggz0[i].wage5-40000)*0.3+5000+3000+450+150+25;
-		}
-		else if(zggz0[i].wage5>20000)
-		{
-			zggz0[i].wage6=(zggz0[i].wage5-20000)*0.25+3000+450+150+25;
-		}
-		else if(zggz0[i].wage5>5000)
-		{
-			zggz0[i].wage6=(zggz0[i].wage5-5000)*0.2+450+150+25;
-		}
-		else if(zggz0[i].wage5>2000)
-		{
-			zggz0[i].wage6=(zggz0[i].wage5-2000)*0.15+150+25;
-		}
-		else if(zggz0[i].wage5>500)
-		{
-			zggz0[i].wage6=(zggz0[i].wage5-500)*0.1+150+25;
-		}
-		else if(zggz0[i].wage5<500)
-		{
-			zggz0[i].wage6=zggz0[i].wage5*0.05;
-		}
-		zggz0[i].wage7=zggz0[i].wage5-zggz0[i].wage6;//计算实得工资
 	
+	}
+
+	}
+	
+	
+    menu();
+
+  
+
 }
+int grsds(float a)
+{
+	float i;
+	if(a<500)
+		i=a-a*0.05;   
+	else if(a>500&&a<2000)
+		i=(a-500)*0.1+25;
+	else if(a>2000&&a<5000)
+		i=(a-2000)*0.15+150+25;
+	else if(a>5000&&a<20000)
+		i=(a-5000)*0.2+450+150+25;
+	else if(a>20000&&a<40000)
+		i=(a-20000)*0.25+3000+450+150+25;
+	else if(a>40000&&a<60000)
+		i=(a-40000)*0.3+5000+3000+450+150+25;
+	else if(a>60000&&a<80000)
+		i=(a-60000)*0.35+6000+5000+3000+450+150+25;
+	else if(a>80000&&a<100000)
+		i=(a-80000)*0.4+7000+6000+5000+3000+450+150+25;
+	else if(a>100000)
+		i=(a-100000)*0.45+8000+7000+6000+5000+3000+450+150+25;
+	return i;
+}
+
